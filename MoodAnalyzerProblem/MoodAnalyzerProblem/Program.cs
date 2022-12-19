1921574ab41166738751311ed78548b06f8b50f3
Nitin308 <nitin3081998@gmail.com>
2022-12-19 19:53:35 +0530

    Test Cases for Happy and Sad

---
+++ MoodAnalyzerProblem/MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MoodAnalyzerProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Program");
+        }
+    }
+
+    public class MoodAnalyzer
+    {
+        public string AnalyzeMood(string message)
+        {
+            var mood1 = message.Contains("Happy", StringComparison.OrdinalIgnoreCase);
+            if (mood1)
+            {
+                return "HAPPY";
+            }
+            var mood2 = message.Contains("Sad", StringComparison.OrdinalIgnoreCase);
+            if (mood2)
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+            //return String.Empty;
+        }
+    }
+}
